ca390c2b8ed680e569579277827ad00fec8a04ff
daniliuskosmaches <nasarspektrreas@gmail.com>
2025-11-02 23:06:21 +0500

    R.Saifullin spring

--- src/main/java/com/example/emailsender/Controller/EmailController.java
+++ src/main/java/com/example/emailsender/Controller/EmailController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api/booking")
 public class EmailController {
 
     private final EmailService emailService;
